4653a0982d9f865d85970e6b6abe96c5bc14d684
Doug Hoard <doug.hoard@gmail.com>
2016-10-23 16:07:08 -0400

    Refactored code to move HTTP get into core class. Renamed core class

--- src/test/java/com/devopology/tools/test/Example2.java
+++ src/test/java/com/devopology/tools/test/Example2.java
@@ -1,24 +1,23 @@
 package com.devopology.tools.test;
 
-import com.devopology.tools.HttpToolset;
+import com.devopology.tools.Toolset;
+import com.devopology.tools.HttpResponse;
 
 /**
  * Created by Doug on 10/22/2016.
  */
-public class Example2 extends HttpToolset {
+public class Example2 extends Toolset {
     public static void main(String [] args) throws Exception {
         new Example2().run(args);
     }
 
     public void run(String [] args) throws Exception {
         println("Let's go ...");
-        //String content = doGet("https://www.google.com/");
-        //println(content);
-
-        setConfiguration(HttpToolset.ACCEPT_INVALID_SSL_CERTIFICATE, "true");
-        String content = doGet("https://tv.eurosport.com/");
-        println(content);
-
+        //String getContent = doGet("https://www.google.com/");
+        //println(getContent);
+        setConfiguration(ACCEPT_INVALID_SSL_CERTIFICATE, "true");
+        HttpResponse httpResponse = doGet("https://www.selfsignedcertificate.com/");
+        println(httpResponse.toString());
         println("Done.");
     }
 }
