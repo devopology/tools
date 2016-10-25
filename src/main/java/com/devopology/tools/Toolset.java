f3c895c6c24a913b44536c6b5788b78ca9648106
Doug Hoard <doug.hoard@gmail.com>
2016-10-24 23:49:56 -0400

    Added additional methods

--- src/main/java/com/devopology/tools/Toolset.java
+++ src/main/java/com/devopology/tools/Toolset.java
@@ -103,7 +103,7 @@ public class Toolset {
         return getClass().getName();
     }
 
-    private File absoluteFile(String path) throws Exception {
+    public File absoluteFile(String path) throws Exception {
         File file = new File(path);
         if (!file.isAbsolute()) {
             file = new File(pwd() + File.separator + path);
@@ -253,7 +253,7 @@ public class Toolset {
             throw new IOException(path + " isn't a directory");
         }
 
-        this.currentDirectory = file;
+        this.currentDirectory = file.getAbsoluteFile();
     }
 
     /**
@@ -367,6 +367,67 @@ public class Toolset {
         else {
             output("type( " + file.getCanonicalPath() + " ) = FILE");
             return FILE;
+        }
+    }
+
+    /**
+     * Method to rename a file
+     *
+     * @param oldPath
+     * @param newPath
+     * @throws Exception
+     */
+    public void rename(String oldPath, String newPath) throws Exception {
+        rename(absoluteFile(oldPath), absoluteFile(newPath));
+    }
+
+    /**
+     * Method to rename a file
+     *
+     * @param oldFile
+     * @param newFile
+     * @throws Exception
+     */
+    public void rename(File oldFile, File newFile) throws Exception {
+        oldFile = absoluteFile(oldFile);
+        newFile = absoluteFile(newFile);
+        oldFile.renameTo(newFile);
+    }
+
+    /**
+     * Method to recursive list all children, grandchildren, etc.
+     *
+     * @param path
+     * @return
+     * @throws Exception
+     */
+    public List<File> recurivelyListFiles(String path) throws Exception {
+        List<File> result = new ArrayList<File>();
+
+        File file = absoluteFile(path);
+        if (file.isDirectory()) {
+            File [] children = file.listFiles();
+            if (null != children) {
+                for (File child : children) {
+                    walkTree(absoluteFile(child), result);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private void walkTree(File file, List<File> list) throws Exception {
+        if (file.isFile()) {
+            list.add(file);
+        }
+        else {
+            File [] children = file.listFiles();
+            if (null != children) {
+                for (File child : children) {
+                    walkTree(absoluteFile(child), list);
+                }
+            }
         }
     }
 
