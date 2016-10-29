0c1aef3f93c66f6fffd5dd31ad9d88756a0ebce8
devopology-git <devopology.git@gmail.com>
2016-10-29 00:37:35 -0400

    Added Javadoc

--- src/main/java/org/devopology/tools/NetworkUtils.java
+++ src/main/java/org/devopology/tools/NetworkUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Doug Hoard
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.devopology.tools;
 
 import java.io.IOException;
@@ -5,16 +21,24 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 
 /**
- * Created by Doug on 10/28/2016.
+ * Class to implement NetworkUtils
  */
 public class NetworkUtils {
 
     private Toolset toolset = null;
 
-    public NetworkUtils(Toolset toolset) {
+    NetworkUtils(Toolset toolset) {
         this.toolset = toolset;
     }
 
+    /**
+     * Method to determine if a TCP/IP connection to a socket can connect
+     *
+     * @param hostname
+     * @param port
+     * @return boolean
+     * @throws IOException
+     */
     public boolean canConnect(String hostname, int port) throws IOException {
         try {
             Socket socket = new Socket();
--- src/main/java/org/devopology/tools/ZipUtils.java
+++ src/main/java/org/devopology/tools/ZipUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Doug Hoard
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.devopology.tools;
 
 import net.lingala.zip4j.core.ZipFile;
@@ -7,14 +23,14 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * Created by Doug on 10/28/2016.
+ * Class to implement ZipUtils
  */
 public class ZipUtils {
 
     protected Toolset toolset = null;
     protected CurrentDirectory currentDirectory = null;
 
-    public ZipUtils(Toolset toolset) {
+    ZipUtils(Toolset toolset) {
         this.toolset = toolset;
         this.currentDirectory = toolset.getCurrentDirectory();
 
